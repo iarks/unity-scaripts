c6f746b951f20125bc5976a11e0b7b51d68dacc0
Arkadeep <arkadeepsaha11@gmail.com>
2019-09-14 17:31:28 +0530

    feat: Initial Commit
    - Added a script to detect pinch gestures and scale gameobject uniformly
    - Added a script to detect left/ right swips and rotate gameobjects
    - Game object rotation is done on the Vector3.up or Vector3.down axes

---
+++ PinchToScale.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Attach to gameobject to enable it to be scaled by pinch gesture
+/// </summary>
+public class PinchToScale : MonoBehaviour
+{
+    [SerializeField]
+    private float _scaleSpeed = 1;
+
+    [SerializeField]
+    private float _maxScale = 5;
+
+    [SerializeField]
+    private float _minScale = 0.5f;
+
+    void Update()
+    {
+        if (Input.touchCount == 2)
+        {
+            // store touches
+            Touch touch1 = Input.GetTouch(0);
+            Touch touch2 = Input.GetTouch(1);
+
+            // find the position in the previous frame of each touch.
+            Vector2 touchOnePrevPos = touch1.position - touch1.deltaPosition;
+            Vector2 touchTwoPrevPos = touch2.position - touch2.deltaPosition;
+
+            // find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchOnePrevPos - touchTwoPrevPos).magnitude;
+            float touchDeltaMag = (touch1.position - touch2.position).magnitude;
+
+            // find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            transform.localScale = transform.localScale - (deltaMagnitudeDiff*_scaleSpeed)*(Vector3.one);
+
+            if (transform.localScale.x < _minScale)
+            {
+                transform.localScale = Vector3.one * _minScale;
+                return;
+            }
+                
+            if (transform.localScale.x > _maxScale)
+            {
+                transform.localScale = Vector3.one * _maxScale;
+                return;
+            }
+        }
+    }
+}
---
+++ SwipeToRotate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Attach To a gameobject to enable it to be rotated by left/ right swipe
+/// </summary>
+public class SwipeToRotate : MonoBehaviour
+{
+    private Touch _touch;
+
+    private Quaternion _yRotation;
+
+    [SerializeField]
+    private float _rotationSpeed = 0.1f;
+
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            _touch = Input.GetTouch(0);
+
+            if (_touch.phase == TouchPhase.Moved)
+            {
+                _yRotation = Quaternion.Euler(0f, -_touch.deltaPosition.x * _rotationSpeed, 0f);
+
+                transform.rotation = _yRotation * transform.rotation;
+            }
+        }
+    }
+}
